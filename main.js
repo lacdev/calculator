53e598409497edf276e683d4b76f0889d4c3ff20
German <german.mtz@protonmail.com>
2021-10-30 20:01:23 -0500

    add event listener, checforoperations and operate functions

--- main.js
+++ main.js
@@ -86,3 +86,74 @@ const clearTemporalOperations = () => {
     operator = ""
 }
 
+const operate = (num1, selectedOperation, num2)=> {
+
+    num1 = operand1
+    selectedOperation = operator
+    num2 = operand2
+
+    if (selectedOperation === "+") {
+        add(num1, num2)
+        display.textContent = operand1
+    }
+
+    if (selectedOperation === "-") {
+        substract(num1, num2)
+        display.textContent = operand1
+    }
+
+    if (selectedOperation === "*") {
+        multiply(num1, num2)
+        display.textContent = operand1
+    }
+
+    if (selectedOperation === "/" && num2 !== 0) {
+        divide(num1, num2)
+        display.textContent = operand1
+    } 
+    
+    if (selectedOperation === "/" && num2 == 0) {
+        clearOperations()
+        temp = "Can't divide by 0. Start again by pressing AC."
+        display.textContent = temp
+    }
+
+}
+
+const checkForEvents = (event) => {
+    if (event.target.classList.contains('number')) {
+        concatValue(event)
+    }
+
+    if (event.target.classList.contains('operator')) {
+        selectOperator(event)
+        if (operand1 === "") {
+            operand1 = temp
+            temp = ""
+        }
+        display.textContent = temp
+    }
+
+    if (event.target.value === 'delete') {
+        deleteValue()
+    }   
+
+    if (event.target.value === "." && (!temp.includes(".") && !operand2.includes("."))) {
+        addDecimal(event)
+    }
+    
+    if (event.target.value === 'clear') {
+        clearOperations()
+    }
+
+    if (event.target.value === 'equals') {
+        if (operator === "" || operand2 === "") {
+            clearOperations()
+            display.textContent = 'Invalid operation start again by pressing AC.'
+        } else {
+        operate(operand1, operator, operand2)
+        }
+    }
+}
+
+calculatorButtons.addEventListener('click', checkForEvents)
