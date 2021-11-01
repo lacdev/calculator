ca67867152fe875b370024aa0be7635ae8090c8a
German <german.mtz@protonmail.com>
2021-10-31 20:27:46 -0600

    fix bugs with overflow and divide by zero also fix bugs on operator chaining

--- main.js
+++ main.js
@@ -6,84 +6,127 @@ let operand2 = ""
 let operator = ""
 let temp = ""
 
+
 const add = (num1, num2) => {
-    temp = Number(num1) + Number(num2)
+    operand1 = parseInt(num1) + parseInt(num2)
     clearTemporalOperations()
+    logCurrentState()
     return operand1
 }
 
 const substract = (num1, num2) => {
-    temp = Number(num1) - Number(num2)
+    operand1 = parseInt(num1) - parseInt(num2)
     clearTemporalOperations()
+    logCurrentState()
     return operand1
 }
 
 const multiply = (num1, num2) => {
-    temp = Number(num1) * Number(num2)
+    operand1 = parseInt(num1) * parseInt(num2)
     clearTemporalOperations()
+    logCurrentState()
     return operand1
 }
 
 const divide = (num1, num2) => {
-        temp = Number(num1) / Number(num2)
+
+    if (num2 == 0) {
+        clearOperations()
+        display.textContent = "Can't divide by 0. Start again."
+        logCurrentState()
+        return
+    } else {
+        operand1 = parseInt(num1) / parseInt(num2)
         clearTemporalOperations()
+        logCurrentState()
         return operand1
+    }
+        
 }
 
 const concatValue = (event) => {
-    if (operand1 !== "") {
-        operand2 += event.target.value
-        display.textContent = operand2
-    } else {
         temp += event.target.value
         display.textContent = temp
-    }
+        logCurrentState()
+
 }
 
 const selectOperator = (event) => {
-    operator = event.target.value
+    if (temp === "" && operand1 === "" && operand2 === "") {
+        display.textContent = "Error, please select a number first."
+        logCurrentState()
+        return
+    } 
+
+    else if (temp !== "" && operand1 === "" && operand2 === ""){
+        operand1 = temp
+        temp = ""
+        operator = event.target.value
+        logCurrentState()
+        return
+    }
+
+    else if (operand1 !== "" && temp === "" && operand2 === ""){
+        operator = event.target.value
+        logCurrentState()
+        return
+    }
+
+    else if (operand1 !== "" && temp !== "" && operand2 === ""){
+        operand2 = temp
+        temp = ""
+        operate(operand1, operator, operand2)
+        operator = event.target.value
+        logCurrentState()
+        return
+    }
 }
 
 const addDecimal = (event) => {
-
-    if (operand1 !== "") {
-        operand2 += event.target.value
-        display.textContent = operand2
-    } else {
-        temp += event.target.value
-        display.textContent = temp
-    }
-
+         if (!temp.includes(".")) {
+             temp += event.target.value
+          } 
+        display.textContent = temp   
 }
 
 const deleteValue = () => {
-
-    if (operand1 !== "") {
-        operand2 = operand2.slice(0, -1)
-        display.textContent = operand2
-    } else {
-        temp = temp.slice(0, -1)
-        display.textContent = temp
-    }
-
+    temp = temp.slice(0, -1)
+    display.textContent = temp
+    logCurrentState()
 }
 
 const clearOperations = () => {
     operand1 = ""
     operand2 = ""
-    temp = ""
     operator = ""
+    temp = ""
     display.textContent = temp
+    logCurrentState()
 }
 
 const clearTemporalOperations = () => {
-    operand1 = temp
     if (!Number.isInteger(operand1)) {
       operand1 = parseFloat(operand1).toFixed(2)
     } 
-    temp = ""
     operand2 = ""
     operator = ""
+    temp = ""
+    display.textContent = operand1
+    logCurrentState()
+}
+
+const checkForEqualSign = () => {
+    if (operand1 !== "" && operand2 === "" && operator !== "" && temp === "") {
+        clearOperations()
+        display.textContent = "Invalid operation. Start again"
+        return
+    } else {
+        operand2 = temp
+        temp = ""
+        operate(operand1, operator, operand2)
+        logCurrentState()
+        return
+    }
 }
 
 const operate = (num1, selectedOperation, num2)=> {
@@ -94,65 +137,59 @@ const operate = (num1, selectedOperation, num2)=> {
 
     if (selectedOperation === "+") {
         add(num1, num2)
-        display.textContent = operand1
     }
 
     if (selectedOperation === "-") {
         substract(num1, num2)
-        display.textContent = operand1
     }
 
     if (selectedOperation === "*") {
         multiply(num1, num2)
-        display.textContent = operand1
     }
 
-    if (selectedOperation === "/" && num2 !== 0) {
+    if (selectedOperation === "/") {
         divide(num1, num2)
-        display.textContent = operand1
-    } 
-    
-    if (selectedOperation === "/" && num2 == 0) {
-        clearOperations()
-        temp = "Can't divide by 0. Start again by pressing AC."
-        display.textContent = temp
     }
 
 }
 
+const logCurrentState = () => {
+    console.log(`Op1 ${operand1}, Op2 ${operand2}, SelOp ${operator}, temp ${temp}`)
+}
+
 const checkForEvents = (event) => {
     if (event.target.classList.contains('number')) {
+        logCurrentState()
         concatValue(event)
     }
 
     if (event.target.classList.contains('operator')) {
-        selectOperator(event)
-        if (operand1 === "") {
-            operand1 = temp
-            temp = ""
-        }
-        display.textContent = temp
+        logCurrentState()
+
+        selectOperator(event) 
     }
 
     if (event.target.value === 'delete') {
+        logCurrentState()
+
         deleteValue()
     }   
 
-    if (event.target.value === "." && (!temp.includes(".") && !operand2.includes("."))) {
+    if (event.target.value === ".") {
+        logCurrentState()
+
         addDecimal(event)
     }
     
     if (event.target.value === 'clear') {
+        logCurrentState()
+
         clearOperations()
     }
 
     if (event.target.value === 'equals') {
-        if (operator === "" || operand2 === "") {
-            clearOperations()
-            display.textContent = 'Invalid operation start again by pressing AC.'
-        } else {
-        operate(operand1, operator, operand2)
-        }
+        logCurrentState()
+        checkForEqualSign(event)
     }
 }
 
