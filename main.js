d8859311190180714134c8760d058aca0ee39552
German <german.mtz@protonmail.com>
2021-11-16 10:10:07 -0600

    remove logCurrentState function from mainjs file

--- main.js
+++ main.js
@@ -1,210 +1,171 @@
 const display = document.querySelector('.display')
-const calculatorButtons = document.querySelector(".buttons")
+const calculatorButtons = document.querySelector('.buttons')
 
-let operand1 = ""
-let operand2 = ""
-let operator = ""
-let temp = ""
+let operand1 = ''
+let operand2 = ''
+let operator = ''
+let temp = ''
 
 const add = (num1, num2) => {
-    operand1 = Number(num1) + Number(num2)
-    clearTemporalOperations()
-    logCurrentState()
-    return operand1
+  operand1 = Number(num1) + Number(num2)
+  clearTemporalOperations()
+  return operand1
 }
 
 const substract = (num1, num2) => {
-    operand1 = Number(num1) - Number(num2)
-    clearTemporalOperations()
-    logCurrentState()
-    return operand1
+  operand1 = Number(num1) - Number(num2)
+  clearTemporalOperations()
+  return operand1
 }
 
 const multiply = (num1, num2) => {
-    operand1 = Number(num1) * Number(num2)
-    clearTemporalOperations()
-    logCurrentState()
-    return operand1
+  operand1 = Number(num1) * Number(num2)
+  clearTemporalOperations()
+  return operand1
 }
 
 const divide = (num1, num2) => {
-
-    if (num2 == 0) {
-        clearOperations()
-        display.textContent = "Can't divide by 0. Start again."
-        logCurrentState()
-        return
-    } else {
-        operand1 = Number(num1) / Number(num2)
-        clearTemporalOperations()
-        logCurrentState()
-        return operand1
-    }
-        
+  if (num2 == 0) {
+    clearOperations()
+    display.textContent = "Can't divide by 0. Start again."
+    return
+  } else {
+    operand1 = Number(num1) / Number(num2)
+    clearTemporalOperations()
+    return operand1
+  }
 }
 
 const concatValue = (event) => {
-    if (operand1 !== "" && operand2 === "" && operator === "" && temp === "") {
-        clearOperations()
-        temp += event.target.value
-        display.textContent = temp
-        logCurrentState()
-    } else {
-        temp += event.target.value
-        display.textContent = temp
-        logCurrentState()
-    }
+  if (operand1 !== '' && operand2 === '' && operator === '' && temp === '') {
+    clearOperations()
+    temp += event.target.value
+    display.textContent = temp
+  } else {
+    temp += event.target.value
+    display.textContent = temp
+  }
 }
 
 const selectOperator = (event) => {
-    if (temp === "" && operand1 === "" && operand2 === "") {
-        display.textContent = "Error, please select a number first."
-        logCurrentState()
-        return
-    } 
-
-    else if (temp !== "" && operand1 === "" && operand2 === ""){
-        operand1 = temp
-        temp = ""
-        operator = event.target.value
-        logCurrentState()
-        return
-    }
-
-    else if (operand1 !== "" && temp === "" && operand2 === ""){
-        operator = event.target.value
-        logCurrentState()
-        return
-    }
-
-    else if (operand1 !== "" && temp !== "" && operand2 === ""){
-        operand2 = temp
-        temp = ""
-        operate(operand1, operator, operand2)
-        operator = event.target.value
-        logCurrentState()
-        return
-    }
+  if (temp === '' && operand1 === '' && operand2 === '') {
+    display.textContent = 'Error, please select a number first.'
+    return
+  } else if (temp !== '' && operand1 === '' && operand2 === '') {
+    operand1 = temp
+    temp = ''
+    operator = event.target.value
+    return
+  } else if (operand1 !== '' && temp === '' && operand2 === '') {
+    operator = event.target.value
+    return
+  } else if (operand1 !== '' && temp !== '' && operand2 === '') {
+    operand2 = temp
+    temp = ''
+    operate(operand1, operator, operand2)
+    operator = event.target.value
+    return
+  }
 }
 
 const addDecimal = (event) => {
-         if (!temp.includes(".")) {
-             temp += event.target.value
-          } 
-        display.textContent = temp   
+  if (!temp.includes('.')) {
+    temp += event.target.value
+  }
+  display.textContent = temp
 }
 
 const deleteValue = () => {
-        temp = temp.slice(0, -1)
-        display.textContent = temp
-        logCurrentState()
-        
+  temp = temp.slice(0, -1)
+  display.textContent = temp
 }
 
 const clearOperations = () => {
-    operand1 = ""
-    operand2 = ""
-    operator = ""
-    temp = ""
-    display.textContent = temp
-    logCurrentState()
+  operand1 = ''
+  operand2 = ''
+  operator = ''
+  temp = ''
+  display.textContent = temp
 }
 
 const clearTemporalOperations = () => {
-    if (!Number.isInteger(operand1)) {
-      operand1 = parseFloat(operand1).toFixed(2)
-    } 
-    operand2 = ""
-    operator = ""
-    temp = ""
-    display.textContent = operand1
-    logCurrentState()
+  if (!Number.isInteger(operand1)) {
+    operand1 = parseFloat(operand1).toFixed(2)
+  }
+  operand2 = ''
+  operator = ''
+  temp = ''
+  display.textContent = operand1
 }
 
 const checkForEqualSign = () => {
-    if (operand1 === "" && operand2 === "" && operator === "" && temp === "") {
-        clearOperations()
-        display.textContent = "Invalid operation. Select your first number and start again."
-        return
-    }
-    if (operand1 !== "" && operand2 === "" && operator !== "" && temp === "") {
-        display.textContent = "Please select your second number."
-        return
-    }  
-     if (operand1 === "" && operand2 === "" && operator === "" && temp !== "") {
-        display.textContent = "Please select an operator and a second number."
-        return
-    } else {
-        operand2 = temp
-        temp = ""
-        operate(operand1, operator, operand2)
-        logCurrentState()
-        return
-    }
+  if (operand1 === '' && operand2 === '' && operator === '' && temp === '') {
+    clearOperations()
+    display.textContent =
+      'Invalid operation. Select your first number and start again.'
+    return
+  }
+  if (operand1 !== '' && operand2 === '' && operator !== '' && temp === '') {
+    display.textContent = 'Please select your second number.'
+    return
+  }
+  if (operand1 === '' && operand2 === '' && operator === '' && temp !== '') {
+    display.textContent = 'Please select an operator and a second number.'
+    return
+  } else {
+    operand2 = temp
+    temp = ''
+    operate(operand1, operator, operand2)
+    return
+  }
 }
 
-const operate = (num1, selectedOperation, num2)=> {
+const operate = (num1, selectedOperation, num2) => {
+  num1 = operand1
+  selectedOperation = operator
+  num2 = operand2
 
-    num1 = operand1
-    selectedOperation = operator
-    num2 = operand2
+  if (selectedOperation === '+') {
+    add(num1, num2)
+  }
 
-    if (selectedOperation === "+") {
-        add(num1, num2)
-    }
+  if (selectedOperation === '-') {
+    substract(num1, num2)
+  }
 
-    if (selectedOperation === "-") {
-        substract(num1, num2)
-    }
+  if (selectedOperation === '*') {
+    multiply(num1, num2)
+  }
 
-    if (selectedOperation === "*") {
-        multiply(num1, num2)
-    }
-
-    if (selectedOperation === "/") {
-        divide(num1, num2)
-    }
-
-}
-
-const logCurrentState = () => {
-    console.log(`Op1 ${operand1}, Op2 ${operand2}, SelOp ${operator}, temp ${temp}`)
+  if (selectedOperation === '/') {
+    divide(num1, num2)
+  }
 }
 
 const checkForEvents = (event) => {
-    if (event.target.classList.contains('number')) {
-        logCurrentState()
-        concatValue(event)
-    }
+  if (event.target.classList.contains('number')) {
+    concatValue(event)
+  }
 
-    if (event.target.classList.contains('operator')) {
-        logCurrentState()
+  if (event.target.classList.contains('operator')) {
+    selectOperator(event)
+  }
 
-        selectOperator(event) 
-    }
+  if (event.target.value === 'delete') {
+    deleteValue()
+  }
 
-    if (event.target.value === 'delete') {
-        logCurrentState()
+  if (event.target.value === '.') {
+    addDecimal(event)
+  }
 
-        deleteValue()
-    }   
+  if (event.target.value === 'clear') {
+    clearOperations()
+  }
 
-    if (event.target.value === ".") {
-        logCurrentState()
-
-        addDecimal(event)
-    }
-    
-    if (event.target.value === 'clear') {
-        logCurrentState()
-
-        clearOperations()
-    }
-
-    if (event.target.value === 'equals') {
-        logCurrentState()
-        checkForEqualSign(event)
-    }
+  if (event.target.value === 'equals') {
+    checkForEqualSign(event)
+  }
 }
 
 calculatorButtons.addEventListener('click', checkForEvents)
